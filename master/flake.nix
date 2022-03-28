89778f3d7979959d4050692f89be5c466c78d860
hugosenari <hugosenari@gmail.com>
2022-03-27 21:51:46 -0300

    chore: re index lolcat

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-lolcat-master.ref   = "refs/heads/master";
   inputs.src-lolcat-master.owner = "OHermesJunior";
   inputs.src-lolcat-master.repo  = "lolcat.nim";
-  inputs.src-lolcat-master.dir   = "";
   inputs.src-lolcat-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
